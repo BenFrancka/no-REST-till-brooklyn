a8e7153d1576eaef04f686e5ecfc00457f917a29
BenFrancka <benfrancka@gmail.com>
2021-09-12 18:33:26 -0700

    adds imports to App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Header from '../header/Header';
+import RESTcontainer from '../../containers/RESTcontainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <RESTcontainer />
+    </>
+  );
 }
